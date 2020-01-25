2927e0baa148267d51790cd417a8b3724d9fac4d
218566 <218566@student.pwr.edu.pl>
2020-01-25 15:19:10 +0100

    Połączenie z bazą działa

--- connect_mysql.php
+++ connect_mysql.php
@@ -1,6 +1,6 @@
 <?php
 
-    $host = "localhost:8000";
+    $host = "localhost";
     $db_user = "root";
     $db_password = "ZjebaneHaslo1209";
     $db_name = "musica";
--- logcheck.php
+++ logcheck.php
@@ -2,7 +2,7 @@
 
     require_once "connect_mysql.php";
 
-    $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);  // @ - wyłączenie informacji o błędach
+    $polaczenie = new mysqli($host, $db_user, $db_password, $db_name);  // @ - wyłączenie informacji o błędach
 
     if($polaczenie->connect_errno!=0) {
         echo "ERROR".$polaczenie->connect_errno." Opis:".$polaczenie->connect_error;
@@ -11,7 +11,7 @@
         $login = $_POST['login'];
         $password = $_POST['password'];
 
-        echo "Działam Panie";
+        echo $login." ".$password;
 
         $polaczenie->close();
     }
